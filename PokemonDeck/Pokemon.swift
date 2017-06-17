dd15a8a805bf35037c5ed396b7b55cc41fb217d5
chandan <elitegreen@gmail.com>
2017-06-17 23:03:00 +0200

    Building the layout

---
+++ PokemonDeck/Pokemon.swift
@@ -0,0 +1,42 @@
+//
+//  Pokemon.swift
+//  PokemonDeck
+//
+//  Created by Chandan Sarkar on 17.06.17.
+//  Copyright © 2017 Chandan. All rights reserved.
+//
+
+import Foundation
+
+//Defining Pokemon as a custom datatype
+class Pokemon{
+    
+    //Pokemon class member variables
+    private var _pokemonName: String!
+    private var _pokedexId: Int!
+    
+    //Getter and Setter
+    var pokemonName: String{
+        get{
+            return _pokemonName
+        }
+        set{
+            _pokemonName = newValue
+        }
+    }
+    
+    var pokedexId: Int{
+        get{
+            return _pokedexId
+        }
+        set{
+            _pokedexId = newValue
+        }
+    }
+    
+    //Initialiser for the Pokemon class
+    init(pokemonName: String,pokedexId: Int) {
+        self._pokemonName = pokemonName
+        self._pokedexId = pokedexId
+    }
+}
